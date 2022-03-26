ffef855b22210674bebcf2177ee949bca9207012
JMS1208 <wjsalstjr59@gmail.com>
2022-03-27 02:51:52 +0900

    first commit

---
+++ app/src/main/java/com/jms/a20220327_criminalintent/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jms.a20220327_criminalintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
